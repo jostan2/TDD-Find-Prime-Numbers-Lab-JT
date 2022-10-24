089f42de965175fca09ea41d48912698ebd9bc5a
joshua <joshuahsientan@gmail.com>
2022-10-24 18:18:25 -0500

    Add project files.

---
+++ Prime Numbers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Find_Prime_Numbers_Lab_JT
+{
+    public class Prime_Numbers
+    {
+        public List<int> ListPrimes(int num3)
+        {
+            // Default empty list
+            var result = new List<int>();
+            for (int n = 0; n <= num3; n++)
+            {
+                if (GetPrime(n))
+                {
+                    result.Add(n);
+                }
+            }
+            return result;
+        }
+
+        public bool GetPrime(int num)
+        {
+            
+            int max = (int)(Math.Sqrt(num) + 1);
+
+            if (num < 2)  // Negative Numbers, 0, 1, and evens are instantly out
+            {
+                return false;
+            }
+            if (num < 4)
+            {
+                return true;
+            }
+            if (IsEven(num)) //even numbers cannot be prime
+            {
+                return false; 
+            }
+            for (int n = 3; n <= max; n += 2)
+            {
+                if (IsDivisor(num, n))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsEven(int num2) //if number is even
+        {
+            if (num2 % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsDivisor(int N, int n) //if the number can only be divided by itself
+        {
+            return (N % n == 0);
+        }
+       
+    }
+}
---
+++ TestPrimeNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TDD_Find_Prime_Numbers_Lab_JT
+{
+    public class TestPrimeNumbers
+    {
+        List<int> empty = new List<int>();
+        Prime_Numbers prime = new Prime_Numbers();
+
+        [Fact]
+        public void testtest() //clear
+        {
+        }
+
+        [Fact]
+        public void IfInputIsZero_Result() //clear
+        {
+            // Arrange - Setup the values for the function
+            var Zero = 0;
+
+            // Act - Perform an operation
+            var result = prime.ListPrimes(Zero);
+
+            // Assert - Check for validity
+            Assert.True(empty.Count == result.Count && empty.Count == 0);
+        }
+
+        [Fact]//before this fact,the text explorer works fine.
+        public void IfPrimeNumbersUpTo31()
+        { 
+            var value = 31;
+
+            bool result = prime.GetPrime(value);
+
+            Assert.True(result[0] is 2 && //if the result is equal to the following numbers up to the tenth prime# (31), it is prime.
+                result[1] is 3 &&
+                result[2] is 5 &&
+                result[3] is 7 &&
+                result[4] is 11 &&
+                result[5] is 13 &&
+                result[6] is 17 &&
+                result[7] is 19 &&
+                result[8] is 23 &&
+                result[9] is 29 &&
+                result[10] is 31); //?? how to write/format this so it works? 
+            
+           /* (from i in Enumerable.Range(0, oldValues.Length)
+             where !oldValues[i] && newValues[i]
+             select i).ToList();*/ //????
+
+
+
+        }
+    }
+}
